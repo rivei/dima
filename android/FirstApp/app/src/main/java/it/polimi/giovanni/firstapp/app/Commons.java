45b5a18ef3520dfb5a835f7db65012f0b6d85c46
Giovanni Quattrocchi <giovanniquattrocchi@me.com>
2017-11-13 11:07:42 +0100

    retrofit, gson, dynamic list view population

---
+++ android/FirstApp/app/src/main/java/it/polimi/giovanni/firstapp/app/Commons.java
@@ -0,0 +1,11 @@
+package it.polimi.giovanni.firstapp.app;
+
+/**
+ * Created by giovanniquattrocchi on 09/11/17.
+ */
+
+public class Commons {
+
+    public static final String apiKey = "55ca991c";
+    public static final String hostName = "http://www.omdbapi.com";
+}
